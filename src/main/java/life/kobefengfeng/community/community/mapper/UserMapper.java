ac6cbbfdc44797aa4e5b987edc5147e91a08fc60
sunweifengfeng <365250246@qq.com>
2021-06-03 20:29:19 +0800

    add check out cookies

--- src/main/java/life/kobefengfeng/community/community/mapper/UserMapper.java
+++ src/main/java/life/kobefengfeng/community/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package life.kobefengfeng.community.community.mapper;
 import life.kobefengfeng.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_mod) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtMod})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
